bc51af59c162bbc407e1a99716d725c166723db1
*addsisem* <sisemorea@berea.edu>
2020-02-07 14:31:23 -0500

    Updated to add subreddit

--- Implementation.py
+++ Implementation.py
@@ -18,7 +18,7 @@ def saveSubmissions(subreddit):
         df.loc[i, 'Subreddit'] = sub[i]
         df.loc[i, 'Top'] = title[i]
 
-    df.to_csv('A5.csv')
+    df.to_csv('A5.csv') #FIX
 
 def main():
 
@@ -26,9 +26,19 @@ def main():
                                  client_secret="kI-F1f7g1-cWqujoQYIgwaG6-QE",
                                  username='sisemorea', password='khg=QrekT78335T')
 
-    subreddit = redditInstance.subreddit('all')
+    subreddit = redditInstance.subreddit('gendercritical')
+    subreddit1 = redditInstance.subreddit('MGTOW2')
+    subreddit2 = redditInstance.subreddit('MensRights')
+    subreddit3 = redditInstance.subreddit('itsafetish')
+    subreddit4 = redditInstance.subreddit('terfisaslur')
+    subreddit5 = redditInstance.subreddit('incelswithouthate')
 
     saveSubmissions(subreddit)
+    saveSubmissions(subreddit1)
+    saveSubmissions(subreddit2)
+    saveSubmissions(subreddit3)
+    saveSubmissions(subreddit4)
+    saveSubmissions(subreddit5)
 
 if __name__ == '__main__':
     main()
