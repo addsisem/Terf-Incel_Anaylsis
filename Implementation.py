ea1af9a85dbc15e684ad86168a329551e7d32328
cooperd <cooperd@berea.edu>
2020-03-19 17:20:51 -0400

    Commented each of my new or altered functions so that they are readable.

--- Implementation.py
+++ Implementation.py
@@ -109,6 +109,12 @@ def compareCSVAuth(filename, filename2):
         print(cross[i])
 
 def read_csv():
+    """
+    This function reads each csv we have, compares each word to a special dictionary
+    that is determined based on file name. Retrieves the total number of derogatory terms
+    used in each csv and creates a percentage from it
+    :return:
+    """
 
     # This list of terms is what is categorized as derogetory as applied to the terf subreddits(Gendercritical, terfisaslur, itsafetish)
     terf_terms = ["man", "he", "him", "it", "TIF", "TIM", "TRA", "MRA", "handmaiden", "NAMALT",
@@ -122,62 +128,69 @@ def read_csv():
     files = ['gendercritical.csv', 'MGTOW2.csv', 'MensRights.csv', 'itsafetish.csv', 'terfisaslur.csv',
              'IncelsWithoutHate.csv']
 
+    # Lists that will be used to create a csv
     sub = []
     percentages = []
     total = []
 
 
-    for h in files:
+    for h in files: # Reads through all files
         termcounter = 0
         totalwords = 0
         percentage = 0
-        with open(h, 'r', encoding="UTF8") as csvfile:
+        with open(h, 'r', encoding="UTF8") as csvfile: # Actually opens the files for reading
             reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-            for row in reader:
-                for i in row:
+            for row in reader: # Looks at each line
+                for i in row: # Looks at each word
                     totalwords += 1
                     if h == 'gendercritical.csv' or 'itsafetish.csv' or 'terfisaslur.csv':
+                        # Depending on the csv, a specific dictionary will be used for comparison
                         if i in terf_terms:
                             termcounter += 1
                     elif h == 'MGTOW2.csv' or 'MensRights.csv' or 'IncelsWithoutHate.csv':
                         if i in incel_terms:
                             termcounter += 1
-        percentage = (termcounter / totalwords)
-        percentage = round(percentage, 3)
+        percentage = (termcounter / totalwords) # Basic math to calculate a percentage
+        percentage = round(percentage, 3) # Round the percentage to make it pretty
 
-        sub.append(h)
-        percentages.append(percentage)
-        total.append(termcounter)
+        sub.append(h) # append the filename to a list
+        percentages.append(percentage) # append the percentage to a list
+        total.append(termcounter) # appened the total number of derogatory terms used in the csv to a list
 
+        # Print the results in a readable format
         print("there are " + str(termcounter) + " derogatory terms in " + h)
         print(str(percentage) + "% of the words  in " + h + " are considered derogatory")
 
 
-
+    # Create the Dataframe and send it to the new csv
     data = {'Subreddit': sub, 'Percentage': percentages, 'Total # of terms': total}  # Create dictionary to hold data
     df = pd.DataFrame(data, columns=['Subreddit', 'Percentage', 'Total # of terms'])  # Move data into dataframe
 
     df.to_csv("Percentage.csv")
 
 def Show_results(filename):
+    """
+    This function uses a csv file to create several bar graphs that communicate our findings
+    :param filename:
+    :return:
+    """
 
-    df = pd.read_csv(filename)
+    df = pd.read_csv(filename) # Reads the given csv file so a graph can be made from the data
     plt.figure(0)  # Fourth Figure
-    plt.rc('xtick', labelsize=6)
-    plt.xlabel("Subreddit")
-    plt.ylabel("Percentage of Derogatory terms used")
-    plt.title("Percentage of Derogatory terms used per subreddit")
-    # Code for Bar Graph here
-    plt.bar(df['Subreddit'], df['Percentage'])
+    plt.rc('xtick', labelsize=6) # Sets the font size of the Xlabel
+    plt.xlabel("Subreddit") # Label the X axis
+    plt.ylabel("Percentage of Derogatory terms used") # Label the Y axis
+    plt.title("Percentage of Derogatory terms used per subreddit") # Sets the title for graph
+    plt.bar(df['Subreddit'], df['Percentage']) # uses the read file and the first two columns of data
 
-    plt.figure(1)
+    plt.figure(1) # Creates a second graph
     plt.rc('xtick', labelsize=6)
     plt.xlabel("Subreddit")
     plt.ylabel("Total # of Derogatory terms used")
     plt.title("Total # of Derogatory terms used per subreddit")
-    plt.bar(df['Subreddit'], df['Total # of terms'])
+    plt.bar(df['Subreddit'], df['Total # of terms']) # This line creates a bar graph
 
-    plt.show()
+    plt.show() # Visually represents all graphs that have been created
 
 
 def main():
