fc430698dfbc9747edb29123c1dedabca1ffc6aa
cooperd <cooperd@berea.edu>
2020-03-19 17:11:47 -0400

    Changed the read_csv function so that it already contains the lsit of all csv files and now it uploads all calculations to a new csv file. This new file is read in the Show_results function in order ot create graphs that will make representation of the data much easier and appealing.

--- Implementation.py
+++ Implementation.py
@@ -3,6 +3,7 @@ from praw.models import MoreComments
 import pandas as pd
 from collections import Counter
 import csv
+import matplotlib.pyplot as plt
 
 def saveSubmissions(subreddit, filename):
     """Function to grab comments from the top 24 posts of a subreddit and save them to a CSV file"""
@@ -107,7 +108,7 @@ def compareCSVAuth(filename, filename2):
     for i in range(len(cross)):
         print(cross[i])
 
-def read_csv(file):
+def read_csv():
 
     # This list of terms is what is categorized as derogetory as applied to the terf subreddits(Gendercritical, terfisaslur, itsafetish)
     terf_terms = ["man", "he", "him", "it", "TIF", "TIM", "TRA", "MRA", "handmaiden", "NAMALT",
@@ -118,27 +119,66 @@ def read_csv(file):
                    "alphas", "omegas", "betas", "cucks", "cuck", "Cuck", "stacy", "becky", "Stacy", "Becky", "Transtrender",
                    "Chad", "Betas", "Cucks", "Hypergamy", "Alphas", "Omegas"]
 
-    termcounter = 0
-    totalwords = 0
-    percentage = 0
+    files = ['gendercritical.csv', 'MGTOW2.csv', 'MensRights.csv', 'itsafetish.csv', 'terfisaslur.csv',
+             'IncelsWithoutHate.csv']
 
-    with open(file, 'r', encoding="UTF8") as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        for row in reader:
-            for i in row:
-                totalwords += 1
-                if file == 'gendercritical.csv' or 'itsafetish.csv' or 'terfisaslur.csv':
-                    if i in terf_terms:
-                        termcounter += 1
-                elif file == 'MGTOW2.csv' or 'MensRights.csv' or 'IncelsWithoutHate.csv':
-                    if i in incel_terms:
-                        termcounter += 1
+    sub = []
+    percentages = []
+    total = []
 
-    percentage = (termcounter/totalwords)
-    percentage = round(percentage, 3)
 
-    print("there are " + str(termcounter) + " derogatory terms in " + file)
-    print(str(percentage) + "% of the words  in " + file + " are considered derogatory")
+    for h in files:
+        termcounter = 0
+        totalwords = 0
+        percentage = 0
+        with open(h, 'r', encoding="UTF8") as csvfile:
+            reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            for row in reader:
+                for i in row:
+                    totalwords += 1
+                    if h == 'gendercritical.csv' or 'itsafetish.csv' or 'terfisaslur.csv':
+                        if i in terf_terms:
+                            termcounter += 1
+                    elif h == 'MGTOW2.csv' or 'MensRights.csv' or 'IncelsWithoutHate.csv':
+                        if i in incel_terms:
+                            termcounter += 1
+        percentage = (termcounter / totalwords)
+        percentage = round(percentage, 3)
+
+        sub.append(h)
+        percentages.append(percentage)
+        total.append(termcounter)
+
+        print("there are " + str(termcounter) + " derogatory terms in " + h)
+        print(str(percentage) + "% of the words  in " + h + " are considered derogatory")
+
+
+
+    data = {'Subreddit': sub, 'Percentage': percentages, 'Total # of terms': total}  # Create dictionary to hold data
+    df = pd.DataFrame(data, columns=['Subreddit', 'Percentage', 'Total # of terms'])  # Move data into dataframe
+
+    df.to_csv("Percentage.csv")
+
+def Show_results(filename):
+
+    df = pd.read_csv(filename)
+    plt.figure(0)  # Fourth Figure
+    plt.rc('xtick', labelsize=6)
+    plt.xlabel("Subreddit")
+    plt.ylabel("Percentage of Derogatory terms used")
+    plt.title("Percentage of Derogatory terms used per subreddit")
+    # Code for Bar Graph here
+    plt.bar(df['Subreddit'], df['Percentage'])
+
+    plt.figure(1)
+    plt.rc('xtick', labelsize=6)
+    plt.xlabel("Subreddit")
+    plt.ylabel("Total # of Derogatory terms used")
+    plt.title("Total # of Derogatory terms used per subreddit")
+    plt.bar(df['Subreddit'], df['Total # of terms'])
+
+    plt.show()
+
 
 def main():
 
@@ -163,10 +203,11 @@ def main():
     #read_csv('gendercritical.csv')
     #saveSubmissions(subreddit, files[2])
 
-    compareCSVAuth(authCommFiles[0], authCommFiles[3])
+    #compareCSVAuth(authCommFiles[0], authCommFiles[3])
 
     #for i in files:
-     #   read_csv(i)
+    #read_csv()
+    Show_results('Percentage.csv')
     #saveSubmissions(subreddit, files[2])
 
 
